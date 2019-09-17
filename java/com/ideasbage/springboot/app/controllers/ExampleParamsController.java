568d5da534fe9cee8ac7b6484f3b818685fa71d6
juank2808 <webjcaf022014@gmail.com>
2019-09-18 00:45:24 +0200

    using GetMapping annotation to send values to the controller and give a request (GET method) now using URL and routes with thymeleaf NOW i use more than one parameter to send to the controller and whoe the values, i use too HTTPServlerRequest

--- java/com/ideasbage/springboot/app/controllers/ExampleParamsController.java
+++ java/com/ideasbage/springboot/app/controllers/ExampleParamsController.java
@@ -1,5 +1,7 @@
 package com.ideasbage.springboot.app.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,28 @@ public class ExampleParamsController {
 	public String param(@RequestParam(name="texto",required=false, defaultValue="some value") String texto,Model model) {
 		/*send parameters via URL using the GET method*/
 		model.addAttribute("respuesta","The text sent is: "+texto);
+		return "params/show";
+	}
+	@GetMapping("/mix-params")
+	public String param(@RequestParam String hello ,@RequestParam Integer number,Model model) {
+		/*send more than one parameter via URL using the GET method*/
+		model.addAttribute("respuesta","The text sent is: "+hello+ " and the number is: "+number);
+		return "params/show";
+	}
+	@GetMapping("/mix-params-request")
+	public String param(HttpServletRequest resquest,Model model) {
+		/*send more than one parameter via URL using the GET method*/
+		/*now using HTTPServletRequest to extract the values*/
+		String hello = resquest.getParameter("hello");
+		/*if the valur is different to Integer we use try and catch*/
+		Integer number=null;
+		try {
+			 number = Integer.parseInt(resquest.getParameter("number"));
+		}catch(NumberFormatException e){
+			number=0;
+		}
+		
+		model.addAttribute("respuesta","The text sent is: "+hello+ " and the number is: "+number);
 		return "params/show";
 	}
 

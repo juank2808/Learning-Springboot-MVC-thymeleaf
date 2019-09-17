bb1079db7db7ebb963c4dca6ff4ea4df148c5b76
juank2808 <webjcaf022014@gmail.com>
2019-09-18 00:04:52 +0200

    using GetMapping annotation to send values to the controller and give a request (GET method)

---
+++ java/com/ideasbage/springboot/app/controllers/ExampleParamsController.java
@@ -0,0 +1,22 @@
+package com.ideasbage.springboot.app.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/params")
+public class ExampleParamsController {
+	
+	//send to the controller data 
+	@GetMapping("/string")
+	/*the param is not required and with default value*/
+	public String param(@RequestParam(name="texto",required=false, defaultValue="some value") String texto,Model model) {
+		/*send parameters via URL using the GET method*/
+		model.addAttribute("respuesta","The text sent is: "+texto);
+		return "params/show";
+	}
+
+}

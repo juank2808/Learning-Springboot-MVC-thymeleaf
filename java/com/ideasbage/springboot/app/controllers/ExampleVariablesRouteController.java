a6accbb66f43e2187c07bb8dd7788ff7749908f2
juank2808 <webjcaf022014@gmail.com>
2019-09-18 03:01:09 +0200

    Using PAth like a parameters but now i send more variables i need the index to understand how is sending from one template or view to  another and manage the Data

--- java/com/ideasbage/springboot/app/controllers/ExampleVariablesRouteController.java
+++ java/com/ideasbage/springboot/app/controllers/ExampleVariablesRouteController.java
@@ -9,14 +9,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/variables")
 public class ExampleVariablesRouteController {
-
+	
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("title","Send route parameters (@Pathvariable)");
+		return "variables/index";
+	}
 	//here i use the path variable to send values like a parameter, is similar like a GET method
 	@GetMapping("/string/{text}")
 	public String variables(@PathVariable String text, Model model) {
 		model.addAttribute("title","Receive route parameters (@Pathvariable)");
-		
-		//
 		model.addAttribute("result", "The text sent in the route is: "+text);
+		
+		//i redirect the result to the template show-variables
+		return "variables/show-variables";
+	}
+	//the same but with more variables
+	@GetMapping("/string/{text}/{number}")
+	public String variables(@PathVariable String text,@PathVariable int number, Model model) {
+		model.addAttribute("title","Receive route parameters (@Pathvariable)");
+		model.addAttribute("result", "The text sent in the route is: "+text);
+		model.addAttribute("result2", "The number sent in the route is: "+number);
 		
 		//i redirect the result to the template show-variables
 		return "variables/show-variables";
